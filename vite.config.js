b8116fb32d80616ea99eca785b727dda98f8726d
Ján Klimek <klimekjani@gmail.com>
2026-02-09 18:28:46 +0100

    update vite config for all html pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,19 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/cv-klimek/', 
+  base: '/cv-klimek/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        contact: resolve(__dirname, 'contact.html'),
+        projects: resolve(__dirname, 'projects.html'),
+        certificates: resolve(__dirname, 'certificates.html'),
+        main_en: resolve(__dirname, 'en/index.html'),
+        contact_en: resolve(__dirname, 'en/contact.html'),
+        projects_en: resolve(__dirname, 'en/projects.html'),
+        certificates_en: resolve(__dirname, 'en/certificates.html'),       
+      },
+    },
+  },
 })
