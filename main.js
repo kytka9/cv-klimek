62c295976acc6ef55208c4cf77e840d380d3046b
Ján Klimek <klimekjani@gmail.com>
2026-05-09 16:54:12 +0200

    update contact pages sk and en, black icons, hovers, transitions

--- main.js
+++ main.js
@@ -2,3 +2,27 @@ import './style.scss';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap';
 import 'devicon/devicon.min.css';
+
+
+// Kopirovanie v kontaktoch
+function setupClickToCopy(cardId, textId) {
+    const card = document.getElementById(cardId);
+    if (!card) return;
+
+    card.addEventListener('click', () => {
+        const text = document.getElementById(textId).innerText;
+        const isEn = document.documentElement.lang === 'en' || window.location.pathname.includes('/en/');
+        
+        navigator.clipboard.writeText(text).then(() => {
+            const p = card.querySelector('p');
+            const oldText = p.innerText;
+            p.innerText = isEn ? "Copied! ✅" : "Skopírované! ✅";
+            
+            setTimeout(() => { p.innerText = oldText; }, 2000);
+        });
+    });
+}
+
+// Potom už len voláš:
+setupClickToCopy('copy-email', 'email-text');
+setupClickToCopy('copy-discord', 'discord-text');
